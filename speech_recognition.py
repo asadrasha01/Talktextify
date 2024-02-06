eb354f2188d178f836ec378e5a1be57115397c4a
asadrasha01 <74987517+asadrasha01@users.noreply.github.com>
2024-02-06 23:08:42 +0200

    update

--- speech_recognition.py
+++ speech_recognition.py
@@ -2,17 +2,16 @@ import speech_recognition as sr
 
 def recognize_speech():
     recognizer = sr.Recognizer()
-    microphone = sr.Microphone()
 
-    with microphone as source:
-        audio = recognizer.listen(source)
+    with sr.Microphone() as source:
         print("Say something:")
+        audio = recognizer.listen(source)
 
     try:
-        text = recognizer.recognize_google(audio)
+        text = recognizer.recognize_sphinx(audio)
         return text
     except sr.UnknownValueError:
         print("Speech Recognition could not understand audio")
     except sr.RequestError as e:
-        print(f"Could not request results from Google Speech Recognition service; {e}")
+        print(f"An error occurred: {e}")
     return None
